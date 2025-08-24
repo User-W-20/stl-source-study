fc6b39b04d89747a58ac8eae7f336fae6f680216
User-W-20 <2633314831@qq.com>
2025-08-24 16:29:15 +0800

    增加vector的内容

--- 序列式容器/序列式容器/vector.cpp
+++ 序列式容器/序列式容器/vector.cpp
@@ -17,97 +17,105 @@
 
 using namespace std;
 
-int main() {
-	int i;
-	vector<int>iv(2, 9);
-	cout << "size=" << iv.size() << endl;//size=2
-	cout << "capacity=" << iv.capacity() << endl;//capacity=2
-	cout << "------------------------";
-	cout << endl;
+//int main() {
+//	int i;
+//	vector<int>iv(2, 9);
+//	cout << "size=" << iv.size() << endl;//size=2
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=2
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.push_back(1);
+//	cout << "size=" << iv.size() << endl;//size=3
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=4
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.push_back(2);
+//	cout << "size=" << iv.size() << endl;//size=4
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=4
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.push_back(3);
+//	cout << "size=" << iv.size() << endl;//size=5
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.push_back(4);
+//	cout << "size=" << iv.size() << endl;//size=6
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	for (i = 0; i < iv.size(); ++i)
+//		cout << iv[i] << " ";//9 9 1 2 3 4
+//	cout << endl;
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.push_back(5);
+//	cout << "size=" << iv.size() << endl;//size=7
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	for (i = 0; i < iv.size(); ++i)
+//		cout << iv[i] << " ";//9 9 1 2 3 4 5
+//	cout << endl;
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.pop_back();
+//	iv.pop_back();
+//	cout << "size=" << iv.size() << endl;//size=5
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	iv.pop_back();
+//	cout << "size=" << iv.size() << endl;//size=4
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	vector<int>::iterator ivite = find(iv.begin(), iv.end(), 1);
+//	if (ivite != iv.end()) iv.erase(ivite);
+//	cout << "size=" << iv.size() << endl;//size=3
+//	cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	for (i = 0; i < iv.size(); ++i)
+//		cout << iv[i] << " ";//9 9 2
+//	cout << endl;
+//	cout << "------------------------";
+//	cout << endl;
+//
+//	vector<int> ivec = { 1, 2, 4 };
+//	auto ite = find(ivec.begin(), ivec.end(), 2);
+//	if ((ite != ivec.end()))  ivec.insert(ite, 3, 7);
+//
+//		cout << "size=" << iv.size() << endl;//size=6
+//		cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//		for (i = 0; i < ivec.size(); ++i)
+//			cout << ivec[i] << " ";//9 9 2
+//		cout << endl;
+//		cout << "------------------------";
+//		cout << endl;
+//
+//		iv.clear();
+//		cout << "size=" << iv.size() << endl;//size=0
+//		cout << "capacity=" << iv.capacity() << endl;//capacity=8
+//		cout << endl;
+//
+//		return 0;
+//}
 
-	iv.push_back(1);
-	cout << "size=" << iv.size() << endl;//size=3
-	cout << "capacity=" << iv.capacity() << endl;//capacity=4
-	cout << "------------------------";
-	cout << endl;
+//vector默认使用alloc作为空间配置器，另外定义了一个data_allocator，目前是更方便的以元素大小为配置单位
 
-	iv.push_back(2);
-	cout << "size=" << iv.size() << endl;//size=4
-	cout << "capacity=" << iv.capacity() << endl;//capacity=4
-	cout << "------------------------";
-	cout << endl;
+//动态增加大小，并不是在原空间之后接续新空间（因为无法保证原空间之后尚有可供配置的空间），而是以原大小的两倍另外配置一块较大空间，然后将原内容拷贝过来，然后才开始在原内容之后构造新元素，并释放原空间。
 
-	iv.push_back(3);
-	cout << "size=" << iv.size() << endl;//size=5
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
+//对vector的任何操作，一旦引起空间重新配置，指向原vector的所有迭代器就都失效了。
 
-	iv.push_back(4);
-	cout << "size=" << iv.size() << endl;//size=6
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
-
-	for (i = 0; i < iv.size(); ++i)
-		cout << iv[i] << " ";//9 9 1 2 3 4
-	cout << endl;
-	cout << "------------------------";
-	cout << endl;
-
-	iv.push_back(5);
-	cout << "size=" << iv.size() << endl;//size=7
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
-
-	for (i = 0; i < iv.size(); ++i)
-		cout << iv[i] << " ";//9 9 1 2 3 4 5
-	cout << endl;
-	cout << "------------------------";
-	cout << endl;
-
-	iv.pop_back();
-	iv.pop_back();
-	cout << "size=" << iv.size() << endl;//size=5
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
-
-	iv.pop_back();
-	cout << "size=" << iv.size() << endl;//size=4
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
-
-	vector<int>::iterator ivite = find(iv.begin(), iv.end(), 1);
-	if (ivite != iv.end()) iv.erase(ivite);
-	cout << "size=" << iv.size() << endl;//size=3
-	cout << "capacity=" << iv.capacity() << endl;//capacity=8
-	cout << "------------------------";
-	cout << endl;
-
-	for (i = 0; i < iv.size(); ++i)
-		cout << iv[i] << " ";//9 9 2
-	cout << endl;
-	cout << "------------------------";
-	cout << endl;
-
-	vector<int> ivec = { 1, 2, 4 };
-	auto ite = find(ivec.begin(), ivec.end(), 2);
-	if ((ite != ivec.end()))  ivec.insert(ite, 3, 7);
-
-		cout << "size=" << iv.size() << endl;//size=6
-		cout << "capacity=" << iv.capacity() << endl;//capacity=8
-		for (i = 0; i < ivec.size(); ++i)
-			cout << ivec[i] << " ";//9 9 2
-		cout << endl;
-		cout << "------------------------";
-		cout << endl;
-
-		iv.clear();
-		cout << "size=" << iv.size() << endl;//size=0
-		cout << "capacity=" << iv.capacity() << endl;//capacity=8
-		cout << endl;
-
-}
